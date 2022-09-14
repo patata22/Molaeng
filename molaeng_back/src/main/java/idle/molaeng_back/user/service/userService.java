9d9f7ebca09496f9439e1f2baabb7913ff8999ce
parsewon <tpdnjs1825@naver.com>
2022-09-14 16:58:23 +0900

    init : user repository, service, controller 생성

---
+++ molaeng_back/src/main/java/idle/molaeng_back/user/service/userService.java
@@ -0,0 +1,4 @@
+package idle.molaeng_back.user.service;
+
+public interface userService {
+}
---
+++ molaeng_back/src/main/java/idle/molaeng_back/user/service/userServiceImpl.java
@@ -0,0 +1,4 @@
+package idle.molaeng_back.user.service;
+
+public class userServiceImpl implements userService {
+}
