a07ab885afa83bc4987c95ab90b5863f0a9ed5ee
parsewon <tpdnjs1825@naver.com>
2022-09-14 17:43:49 +0900

    init : swagger, web config 추가

--- molaeng_back/src/main/java/idle/molaeng_back/user/model/userRepository.java
+++ molaeng_back/src/main/java/idle/molaeng_back/user/model/userRepository.java
@@ -2,5 +2,5 @@ package idle.molaeng_back.user.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepository extends JpaRepository {
+public interface userRepository extends JpaRepository <User, Integer> {
 }
