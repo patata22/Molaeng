71681c6b865e380a9178552f86d97dda41168302
이지영 <jioung93@naver.com>
2022-09-17 21:19:47 +0900

    [S07P22A604-64] feat: 네이버 쇼핑 검색 API 적용

--- molaeng_back/src/main/java/idle/molaeng_back/MolaengBackApplication.java
+++ molaeng_back/src/main/java/idle/molaeng_back/MolaengBackApplication.java
@@ -2,12 +2,22 @@ package idle.molaeng_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 
 @SpringBootApplication
+@EnableScheduling
 public class MolaengBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MolaengBackApplication.class, args);
+	}
+
+	@Bean
+	public TaskScheduler taskScheduler() {
+		return new ConcurrentTaskScheduler();
 	}
 
 }
