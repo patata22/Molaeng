79a7904b6e5b8128d4295491ee3da44725549b76
이지영 <jioung93@naver.com>
2022-09-21 13:16:27 +0900

    [S07P22A604-119] feat: 재료 검색 페이지 컴포넌트 구현

---
+++ molaeng_front/src/components/molecules/MoleculesIngredientTagList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ingredient-tag
+      v-for="(ingredient, index) in ingredientList"
+      :key="index"
+      :text="ingredient.ingredientName"
+      :id="ingredient.ingredientId"
+    >
+    </ingredient-tag>
+  </div>
+</template>
+
+<script>
+import IngredientTag from "../atoms/AtomsIngredientTag.vue";
+export default {
+  name: "IngredientTagList",
+  components: {
+    IngredientTag,
+  },
+  props: {
+    ingredientList: Array,
+  },
+};
+</script>
+
+<style></style>
