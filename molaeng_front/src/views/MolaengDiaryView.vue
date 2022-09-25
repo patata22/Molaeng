52433df110a25231099bb3f48600554ed1f44aa8
leenak0 <leenak0@naver.com>
2022-09-25 21:07:19 +0900

    feat: 모랭일기 캘린더 component 구현

---
+++ molaeng_front/src/views/MolaengDiaryView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div><molaeng-diary-header /></div>
+    <div><molaeng-calendar /></div>
+    <div><molaeng-graph /></div>
+  </div>
+</template>
+
+<script>
+import MolaengDiaryHeader from "../components/organisms/OrganismsMolaengDiaryHeader.vue";
+import MolaengCalendar from "../components/organisms/OrganismsMolaengCalendar.vue";
+import MolaengGraph from "../components/organisms/OrganismsMolaengGraph.vue";
+export default {
+  name: "MolaengDiaryView",
+  components: {
+    MolaengDiaryHeader,
+    MolaengCalendar,
+    MolaengGraph,
+  },
+  data: () => ({}),
+};
+</script>
+
+<style></style>
