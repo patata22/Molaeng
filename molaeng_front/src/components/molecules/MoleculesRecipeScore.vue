a959c5cf249bcb6c3c2bc6d47c73e70f6ed9c709
김지건 <jigun21@naver.com>
2022-09-27 16:54:07 +0900

    feat: Review-User 연동 및 프런트 페이지 수정

--- molaeng_front/src/components/molecules/MoleculesRecipeScore.vue
+++ molaeng_front/src/components/molecules/MoleculesRecipeScore.vue
@@ -1,106 +1,135 @@
 <template>
-  <div>
+  <v-card outlined outline-color="primary">
     <v-row>
-      <v-col>
-        <h2>평점</h2>
+      <v-col cols="4" class="text-center">
+        <br />
+        <h2>평 점</h2>
+        <br />
         <h2>{{ avgScore }}</h2>
-      </v-col>
-      <div>
+        <br />
         <v-row>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-spacer></v-spacer>
+          <v-icon color="yellow" v-if="avgScore >= 1">mdi-star</v-icon>
+          <v-icon color="yellow" v-else>mdi-star-outline</v-icon>
+          <v-icon color="yellow" v-if="avgScore >= 2">mdi-star</v-icon>
+          <v-icon color="yellow" v-else>mdi-star-outline</v-icon>
+          <v-icon color="yellow" v-if="avgScore >= 3">mdi-star</v-icon>
+          <v-icon color="yellow" v-else>mdi-star-outline</v-icon>
+          <v-icon color="yellow" v-if="avgScore >= 4">mdi-star</v-icon>
+          <v-icon color="yellow" v-else>mdi-star-outline</v-icon>
+          <v-icon color="yellow" v-if="avgScore >= 5">mdi-star</v-icon>
+          <v-icon color="yellow" v-else>mdi-star-outline</v-icon>
+        </v-row>
+      </v-col>
+      <v-col cols="8">
+        <v-row>
+          <v-col cols="5">
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5" align-self="center">
+            <v-progress-linear
+              height="8"
+              rounded
+              background-color="semidark"
+              color="primary"
+              value="50"
+            ></v-progress-linear>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
-          </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
-          </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
-          </v-col>
-          <v-col>
+          <v-col cols="2">
             {{ scoreCnt[4] }}
           </v-col>
         </v-row>
         <v-row>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5">
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5" align-self="center">
+            <v-progress-linear
+              height="8"
+              rounded
+              background-color="semidark"
+              color="primary"
+              value="50"
+            ></v-progress-linear>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="2">
+            {{ scoreCnt[3] }}
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
-          </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>{{ scoreCnt[3] }}</v-col>
         </v-row>
         <v-row>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5">
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5" align-self="center">
+            <v-progress-linear
+              height="8"
+              rounded
+              background-color="semidark"
+              color="primary"
+              value="50"
+            ></v-progress-linear>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="2">
+            {{ scoreCnt[2] }}
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>{{ scoreCnt[2] }}</v-col>
         </v-row>
         <v-row>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5">
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5" align-self="center">
+            <v-progress-linear
+              height="8"
+              rounded
+              background-color="semidark"
+              color="primary"
+              value="50"
+            ></v-progress-linear>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
+          <v-col cols="2">
+            {{ scoreCnt[1] }}
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>{{ scoreCnt[1] }}</v-col>
         </v-row>
         <v-row>
-          <v-col>
-            <v-icon color="yellow">mdi-star</v-icon>
+          <v-col cols="5">
+            <v-icon color="yellow" small>mdi-star</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
+            <v-icon color="yellow" small>mdi-star-outline</v-icon>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
+          <v-col cols="5" align-self="center">
+            <v-progress-linear
+              height="8"
+              rounded
+              background-color="semidark"
+              color="primary"
+              value="50"
+            ></v-progress-linear>
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
+          <v-col cols="2">
+            {{ scoreCnt[0] }}
           </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>
-            <v-icon color="yellow">mdi-star-outline</v-icon>
-          </v-col>
-          <v-col>{{ scoreCnt[0] }}</v-col>
         </v-row>
-      </div>
+      </v-col>
     </v-row>
-  </div>
+  </v-card>
 </template>
 <script>
 import axios from "axios";
