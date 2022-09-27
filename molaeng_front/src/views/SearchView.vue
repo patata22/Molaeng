4db6fc7ac10103533182de540fe2bba7770f56a6
이지영 <jioung93@naver.com>
2022-09-27 09:54:04 +0900

    feat: 메뉴 탭 구현 및 버튼 작동

--- molaeng_front/src/views/SearchView.vue
+++ molaeng_front/src/views/SearchView.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
-    <menu-tab />
-    <div id="content">
-      <selected-tag-list />
-      <ingredient-card-list />
-    </div>
-    <under-bar-button :text="buttonText" />
+    <menu-tab :tabs="tabs" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import MenuTab from "../components/molecules/MoleculesMenuTab.vue";
-import SelectedTagList from "../components/molecules/MoleculesSelectedTagList.vue";
-import IngredientCardList from "../components/templates/TemplatesIngredientCardList.vue";
-import UnderBarButton from "../components/atoms/AtomsUnderBarButton.vue";
+
 export default {
   name: "SearchView",
   components: {
     MenuTab,
-    SelectedTagList,
-    IngredientCardList,
-    UnderBarButton,
   },
   data: () => ({
     buttonText: "레시피 검색",
+    tabs: [
+      {
+        tabName: "재료",
+        tabLink: "/ingredient",
+      },
+      {
+        tabName: "레시피",
+        tabLink: "/recipe",
+      },
+    ],
   }),
 };
 </script>
