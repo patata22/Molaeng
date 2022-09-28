fded0526a6736f8fa65c6aed39fadf84b85d1003
yhk <gmlrud9808@naver.com>
2022-09-28 10:52:57 +0900

    feat : recipe menu tab

---
+++ molaeng_front/src/store/recipe.js
@@ -0,0 +1,17 @@
+import api from "@/api/RestAPI";
+import axios from "axios";
+
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export const recipe = {
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+};
+api;
+axios;
