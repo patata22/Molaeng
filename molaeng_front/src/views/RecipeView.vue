1d58f7665e23dbfa248bb8b5d68fbfccee29b8ae
yhk <gmlrud9808@naver.com>
2022-09-28 11:14:15 +0900

    fix : RecipeView.vue, router 코드 수정

--- molaeng_front/src/views/RecipeView.vue
+++ molaeng_front/src/views/RecipeView.vue
@@ -1,34 +1,23 @@
 <template>
   <div>
     <recipe-detail-header v-bind:recipeInfo="recipeInfo" />
-    <v-tabs centered fixed-tabs>
-      <menu-tab-item
-        v-for="(title, index) in titles"
-        :key="index"
-        :text="title"
-        :requestUrl="title"
-      />
-    </v-tabs>
-    <!-- 임시 -->
-    <button @click="moveReview">리뷰이동</button>
-    <organisms-recipe-description-list />
+    <menu-tab :tabs="tabs" />
+    <router-view></router-view>
     <under-bar-button :text="buttonText" @click.native="moveMolaeng" />
   </div>
 </template>
 
 <script>
 import RecipeDetailHeader from "../components/organisms/OrganismsRecipeDetailHeader.vue";
-import OrganismsRecipeDescriptionList from "@/components/organisms/OrganismsRecipeDescriptionList.vue";
 import UnderBarButton from "../components/atoms/AtomsUnderBarButton.vue";
-import MenuTabItem from "../components/atoms/AtomsMenuTabItem.vue";
+import MenuTab from "../components/molecules/MoleculesMenuTab.vue";
 
 export default {
   name: "RecipeView",
   components: {
     RecipeDetailHeader,
-    OrganismsRecipeDescriptionList,
     UnderBarButton,
-    MenuTabItem,
+    MenuTab,
   },
   data: () => ({
     recipeInfo: {
@@ -39,18 +28,31 @@ export default {
       recipeImg: "http://file.okdab.com/UserFiles/searching/recipe/002400.jpg",
       isLiked: true,
     },
+    tabs: [
+      {
+        tabName: "재료",
+        tabLink: "/recipe/ingInfo",
+      },
+      {
+        tabName: "조리방법",
+        tabLink: "/recipe/description",
+      },
+      {
+        tabName: "가격정보",
+        tabLink: "/",
+      },
+      {
+        tabName: "리뷰",
+        tabLink: "/recipe/review",
+      },
+    ],
     buttonText: "모랭일기에 기록하기",
-    titles: ["재료", "조리방법", "가격정보", "리뷰"],
   }),
   methods: {
     moveMolaeng() {
       this.$router.push({ path: "/diary" });
     },
-    moveReview() {
-      this.$router.push({ path: "/review" });
-    },
   },
 };
 </script>
-
 <style></style>
