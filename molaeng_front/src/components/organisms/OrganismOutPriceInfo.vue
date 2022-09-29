4287a9640514055f947367134401c69bf71416e2
김승준 <joon8445@naver.com>
2022-09-29 23:12:13 +0900

    init: 레시피 가격정보 템플릿 생성, 외식비정보 컴포넌트 생성

---
+++ molaeng_front/src/components/organisms/OrganismOutPriceInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ seoul }}</h1>
+    <h1>{{ my }}</h1>
+    <h1>{{ gugunName }}</h1>
+  </div>
+</template>
+
+<script>
+import API from "@/api/APIs";
+const api = API;
+export default {
+  name: "OutPriceInfo",
+  data() {
+    return {
+      seoul: 0,
+      my: 0,
+      gugunName: "",
+    };
+  },
+  props: {
+    recipeId: String,
+  },
+  created() {
+    api.outPrice(this.recipeId).then((res) => {
+      this.seoul = res.seoul;
+      this.my = res.my;
+      this.gugunName = res.gugunName;
+    });
+  },
+};
+</script>
+
+<style></style>
---
+++ molaeng_front/src/components/templates/TemplatesRecipePriceInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <out-price-info :recipeId="recipeId"></out-price-info>
+  </div>
+</template>
+
+<script>
+import OutPriceInfo from "@/components/organisms/OrganismOutPriceInfo.vue";
+
+export default {
+  name: "RecipePriceInfo",
+  props: {
+    recipeId: String,
+  },
+  components: {
+    OutPriceInfo,
+  },
+};
+</script>
+
+<style></style>
