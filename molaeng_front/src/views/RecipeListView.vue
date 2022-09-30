3b3f78f510d7bbe075bad73b461aa3b85a3d5133
김지건 <jigun21@naver.com>
2022-09-30 18:44:25 +0900

    fix: route etc

--- molaeng_front/src/views/RecipeListView.vue
+++ molaeng_front/src/views/RecipeListView.vue
@@ -1,7 +1,12 @@
 <template lang="">
-  <div>RecipeListView Page</div>
+  <templates-recipe-list></templates-recipe-list>
 </template>
 <script>
-export default {};
+import TemplatesRecipeList from "../components/templates/TemplatesRecipeList.vue";
+export default {
+  components: {
+    TemplatesRecipeList,
+  },
+};
 </script>
 <style lang=""></style>
