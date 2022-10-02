801e9ac3749765b71ffb58eea538fe7ab1a1180e
yhk <gmlrud9808@naver.com>
2022-10-02 19:19:50 +0900

    feat : axios 수정 및 코드 이동, 상단바 고정

--- molaeng_front/src/views/RecipeView.vue
+++ molaeng_front/src/views/RecipeView.vue
@@ -1,35 +1,35 @@
 <template>
   <div>
-    <recipe-detail-header v-bind:recipeInfo="recipeInfo" :recipeId="recipeId" />
-    <menu-tab :tabs="tabs" />
+    <recipe-detail-header-image :recipeInfo="recipeInfo" />
+    <div style="position: sticky; top: 0; z-index: 3; background-color: white">
+      <recipe-detail-header
+        v-bind:recipeInfo="recipeInfo"
+        :recipeId="recipeId"
+      />
+      <menu-tab :tabs="tabs" />
+    </div>
     <router-view :recipeId="recipeId"></router-view>
-    <under-bar-button :text="buttonText" @click.native="moveMolaeng" />
-    <v-dialog v-model="dialog">
-      <add-to-molaeng-button @cancel="cancel" />
-    </v-dialog>
   </div>
 </template>
 
 <script>
+import RecipeDetailHeaderImage from "../components/atoms/AtomsRecipeDetailHeaderImage.vue";
 import RecipeDetailHeader from "../components/organisms/OrganismsRecipeDetailHeader.vue";
-import UnderBarButton from "../components/atoms/AtomsUnderBarButton.vue";
 import MenuTab from "../components/molecules/MoleculesMenuTab.vue";
-import AddToMolaengButton from "../components/atoms/AtomsAddToMolaengButton.vue";
 
-import { mapGetters } from "vuex";
+import API from "@/api/APIs";
+const api = API;
 
 // recipeList에서 레시피아이디를 받아서 axios 요청 보내는 코드 추가해야 함
 // 레시피아이디를 어떻게 받아오는지..?
 export default {
   name: "RecipeView",
   components: {
+    RecipeDetailHeaderImage,
     RecipeDetailHeader,
-    UnderBarButton,
     MenuTab,
-    AddToMolaengButton,
   },
   computed: {
-    ...mapGetters(["recipeInfo"]),
     tabs() {
       return [
         {
@@ -51,33 +51,18 @@ export default {
       ];
     },
   },
-  // mounted() {
-  //   console.log("asdf" + this.recipeInfo);
-  //   console.log(this.tabs);
-  // },
-  created() {
+  async created() {
+    //recipeId 저장
     const pathName = this.$route.fullPath.split("/");
     this.recipeId = pathName[2];
     //레시피 상단바 상세정보
-    this.$store.dispatch("getRecipeInfo", this.recipeId);
-    //레시피 조리방법
-    this.$store.dispatch("getRecipeDetail", this.recipeId);
+    this.recipeInfo = await api.getRecipeInfo(this.recipeId);
   },
   data: () => ({
     recipeId: "",
-    buttonText: "모랭일기에 기록하기",
-    dialog: false,
+    //레시피 상단바 상세정보
+    recipeInfo: {},
   }),
-  methods: {
-    moveMolaeng() {
-      // this.$router.push({ path: "/diary" });
-      this.dialog = true;
-    },
-    //emit으로 AddToMolaengButton에게서 전달받은 dialog
-    cancel(value) {
-      this.dialog = value;
-    },
-  },
 };
 </script>
 <style></style>
