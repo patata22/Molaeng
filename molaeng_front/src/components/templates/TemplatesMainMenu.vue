b06afb7fe4a97992ef0e5924d500de8efb5ac56f
김지건 <jigun21@naver.com>
2022-10-03 13:51:15 +0900

    메인메뉴 라우터 수정

--- molaeng_front/src/components/templates/TemplatesMainMenu.vue
+++ molaeng_front/src/components/templates/TemplatesMainMenu.vue
@@ -84,7 +84,7 @@ export default {
       this.$router.push("/search").catch(() => {});
     },
     goRecipeSearch() {
-      this.$router.push("/search").catch(() => {});
+      this.$router.push("/search/recipe").catch(() => {});
     },
   },
 };
