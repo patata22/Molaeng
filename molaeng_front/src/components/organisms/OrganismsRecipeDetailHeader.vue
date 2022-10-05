88ff6f80b4f12fe6104ffd2a71e1f8d68a54aab6
yhk <gmlrud9808@naver.com>
2022-10-05 10:30:58 +0900

    fix: [FE] 재료태그 가로스크롤 수정

--- molaeng_front/src/components/organisms/OrganismsRecipeDetailHeader.vue
+++ molaeng_front/src/components/organisms/OrganismsRecipeDetailHeader.vue
@@ -11,7 +11,7 @@
           >mdi-cards-heart-outline</v-icon
         >
       </div>
-      <div style="text-align: center">
+      <div style="text-align: center" class="type1">
         <div style="color: #5b574b; font-size: x-large; font-weight: bold">
           {{ recipeInfo.recipeName }}
         </div>
@@ -20,12 +20,6 @@
         </div>
         <recipe-ingredient-tag-list
           :ingredientList="recipeIngredientList"
-          style="
-            display: flex;
-            flex-wrap: nowrap;
-            overflow-x: auto;
-            margin: 3% 5% 0 5%;
-          "
           class="box"
         />
       </div>
@@ -77,10 +71,18 @@ display: flex;
 flex-wrap: nowrap;
 overflow-x: auto; */
 .box {
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
+  overflow: auto;
+  white-space: nowrap;
+  margin: 3% 5% 3% 5%;
 }
-.box::-webkit-scrollbar {
-  display: none; /* Chrome, Safari, Opera*/
+::-webkit-scrollbar {
+  height: 10px;
+}
+::-webkit-scrollbar-track {
+  background: white;
+}
+::-webkit-scrollbar-thumb {
+  background: #fdac6d;
+  border-radius: 10px;
 }
 </style>
